ce88e1d0bca53e8f4e84072a93734748f7966b1e
Pallab Kashyap <pallabkshyp@gmail.com>
2025-12-09 23:56:52 +0530

    Update Google Maps iframe source in Footer component

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
   return (
     <div className="overflow-hidden rounded-xl flex-1">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15215.986943133721!2d78.3448502!3d17.5553421!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb8ddfba9644c9%3A0x21080c4023d9f489!2sRavi%20Bio%20Fuel%20Industries!5e0!3m2!1sen!2sin!4v1732715301521!5m2!1sen!2sin"
+        src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3802.2045176249485!2d78.55074400000001!3d17.64047!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTfCsDM4JzI1LjciTiA3OMKwMzMnMDIuNyJF!5e0!3m2!1sen!2sin!4v1765304712510!5m2!1sen!2sin"
         className="h-[60vh] sm:h-full w-full"
         style={{ border: "0" }}
         loading="lazy"
